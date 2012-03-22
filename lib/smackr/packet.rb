85e8037386a457a7973e9ef9c8ae34d2bb0018c0
Jack Thorne <jthorne@where.com>
2012-03-22 13:30:11 -0400

    remove type objects and working init

--- lib/smackr/packet.rb
+++ lib/smackr/packet.rb
@@ -11,63 +11,20 @@ class Packet
     }
 
     def initialize(opts={})
-      unless opts[:message_type]
-        :message_type = 'normal'
-        #raise ArgumentError, ':message_type is required'
+      unless
+        unless opts[:message_type]
+          :message_type = 'normal'
+        end
+        else if :message_type == 'error'
+          if opts[:subject] || opts[:thread] || opts[:body]
+            raise ArgumentError, ':error can be the only argument in an error message'
+          end
+        end
+
       end
     end
 
-    class Normal
-      attr_accessor :subject, :thread, :body, :error
-      def initialize
-        if opts[:error]
-          raise ArgumentError, ':error cannont be used in a normal message'
-        end
-        if opts[:subject] and opts[:body]
-          raise ArgumentError, 'either :subject or :body is required in normal message'
-        end
-      end
     end
-
-    class Chat
-      attr_accessor :subject, :thread, :body, :error
-      def initialize
-        if opts[:error]
-          raise ArgumentError, ':error cannont be used in a normal message'
-        end
-        if opts[:thread] and opts[:body]
-          raise ArgumentError, 'either :thread or :body is required in normal message'
-        end
-      end
-    end
-
-    class GroupChat
-      attr_accessor :subject, :thread, :body, :error
-      def initialize
-        if opts[:error]
-          raise ArgumentError, ':error cannont be used in a normal message'
-        end
-      end
-    end
-
-    class Headline
-      attr_accessor :subject, :thread, :body, :error
-      def initialize
-        if opts[:error]
-          raise ArgumentError, ':error cannont be used in a normal message'
-        end
-      end
-    end
-
-    class Error
-      attr_accessor :subject, :thread, :body, :error
-      def initialize
-        unless opts[:error]
-          raise ArgumentError, ':error is required'
-        end
-      end
-    end
-  end
   class Presence
 
   end
