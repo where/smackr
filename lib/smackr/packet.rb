090f4c91229f108df1f8d55a758c60514fa6fdc8
Jack Thorne <javajax@gmail.com>
2012-03-25 15:59:47 -0400

    working on tests

--- lib/smackr/packet.rb
+++ lib/smackr/packet.rb
@@ -1,26 +1,28 @@
 class Packet
   class Message
     attr_accessor :message_type, :subject, :thread, :body, :error
-
-    def initialize(opts={})
+    
+    def initialize
       puts 'message init started'
-      #unless opts[:message_type]
-        #:message_type = 'normal'
-      #end
-      if :message_type == 'error'
-        if opts[:subject] || opts[:thread] || opts[:body]
-          raise ArgumentError, ':error can be the only argument in an error message'
-        end
-      end
-
-      @message_type = opts[:message_type]
-      @subject = opts[:subject]
-      @thread = opts[:thread]
-      @body = opts[:body]
-      @error = opts[:error]
+      # if :message_type == 'error'
+      #   if opts[:subject] || opts[:thread] || opts[:body]
+      #     raise ArgumentError, ':error can be the only argument in an error message'
+      #   end
+      # end
+      # 
+      # @message_type = opts[:message_type]
+      # @subject = opts[:subject]
+      # @thread = opts[:thread]
+      # @body = opts[:body]
+      # @error = opts[:error]
+      
+      # self.message_type = opts[:message_type]
+      # self.subject = opts[:subject]
+      # self.thread = opts[:thread]
+      # self.body = opts[:body]
+      # self.error = opts[:error]
     end
-
-    end
+  end
   class Presence
   end
   class IQ
--- test/test_message.rb
+++ test/test_message.rb
@@ -1,6 +1,9 @@
 require 'helper'
 
 class TestMessage < Test::Unit::TestCase
+  should "The Truth" do
+    assert true
+  end
   should "Normal message create" do
     message = Message.new
     assert message
